7e14197fafbeaca6b4179a7fab9f41de07266c1f
Jordan <jordan.benson4@gmail.com>
2022-02-10 19:07:55 -0800

    Create query for returning properties containing search parameters as well as returning all a user's properties when passed a user id.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -70,17 +70,68 @@ exports.getAllReservations = getAllReservations;
 
 /**
  * Get all properties.
- * @param {{}} options An object containing query options.
- * @param {*} limit The number of results to return.
- * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE 1 = 1
+  `;
   
-  return pool.query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => result.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
+  if (options.owner_id) {
+    queryParams.push(Number(options.owner_id));
+    queryString += `AND owner_id = $${queryParams.length} `;
+  }
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `AND city LIKE $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += `AND cost_per_night > $${queryParams.length} `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `AND cost_per_night < $${queryParams.length} `;
+  }
+
+  queryString += `
+  GROUP BY properties.id
+  `;
+  
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating)
+    queryString += `
+    HAVING avg(property_reviews.rating) >= $${queryParams.length}
+    `;
+  } 
+
+  queryString += `
+  ORDER BY cost_per_night
+  `;
+
+  queryParams.push(limit);
+  queryString += `
+  LIMIT $${queryParams.length}
+  `;
+
+  console.log('query: ', queryString);
+  console.log('params: ', queryParams);
+
+  return pool.query(queryString, queryParams)
+  .then((result) => {
+    console.log('res:', result);
+    return result.rows
+  })
+  .catch((err) => {
+    console.log('err: ', err);
+    return null
+  });
 }
 exports.getAllProperties = getAllProperties;
 
@@ -91,9 +142,6 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  
 }
 exports.addProperty = addProperty;
